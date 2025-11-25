ae74be12bfbdf556a9deaa4eaf3e2b15f91c909b
Script-Savant <alexkienjeku@gmail.com>
2025-11-25 22:37:59 +0300

    recipe data model

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,4 +10,12 @@ func main() {
 	router := gin.Default()
 
 	router.Run()
+}
+
+type Recipe struct {
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
 }
