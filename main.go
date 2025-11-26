2a165072c1455349c93ce2273d1fc4a765a386e5
Script-Savant <alexkienjeku@gmail.com>
2025-11-26 22:59:32 +0300

    search recipe by tag

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -34,6 +35,7 @@ func main() {
 	router.GET("/recipes", ListRecipesHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
 	router.DELETE("/recipes/:id", DeleteRecipeHandler)
+	router.GET("/recipes/search", SearchRecipesHandler)
 
 	router.Run()
 }
@@ -99,4 +101,22 @@ func DeleteRecipeHandler(c *gin.Context) {
 	recipes = append(recipes[:index], recipes[index+1:]...)
 
 	c.JSON(http.StatusOK, gin.H{"message": "Recipe has been deleted"})
+}
+
+func SearchRecipesHandler(c *gin.Context) {
+	tag := c.Query("tag")
+	var listOfRecipes []Recipe
+
+	for i := 0; i < len(recipes); i++ {
+		found := false
+		for _, t := range recipes[i].Tags {
+			if strings.Contains(t, tag) {
+				found = true
+			}
+		}
+		if found {
+			listOfRecipes = append(listOfRecipes, recipes[i])
+		}
+	}
+	c.JSON(http.StatusOK, listOfRecipes)
 }
